f7920664907c3545ac736c14d645e694b801e4df
Yusak Abiyyu <yusakabiyyu@gmail.com>
2026-04-05 22:51:41 +0700

    Menambahkan Class SayaMusicTrack

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using TP_MODUL6_103022400040;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaMusicTrack lagu1 = new SayaMusicTrack("Lagu A");
+        SayaMusicTrack lagu2 = new SayaMusicTrack("Lagu B");
+
+        lagu1.IncreasePlayCount(10);
+        lagu2.IncreasePlayCount(5);
+
+        lagu1.PrintTrackDetails();
+        lagu2.PrintTrackDetails();
+    }
+}
---
+++ SayaMusicTrack.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP_MODUL6_103022400040
+{
+
+    using System;
+
+    public class SayaMusicTrack
+    {
+        private int id;
+        private int playCount;
+        private string title;
+
+        // Constructor
+        public SayaMusicTrack(string title)
+        {
+            this.title = title;
+            this.playCount = 0;
+
+            // generate id random 5 digit
+            Random rand = new Random();
+            this.id = rand.Next(10000, 100000);
+        }
+
+        // Method untuk menambah play count
+        public void IncreasePlayCount(int count)
+        {
+            this.playCount += count;
+        }
+
+        // Method untuk print detail
+        public void PrintTrackDetails()
+        {
+            Console.WriteLine("ID: " + id);
+            Console.WriteLine("Title: " + title);
+            Console.WriteLine("Play Count: " + playCount);
+            Console.WriteLine("----------------------");
+        }
+    }
+}
